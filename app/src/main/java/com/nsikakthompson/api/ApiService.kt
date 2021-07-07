2da79d4ca0604369bc6466152db36c7e13bdc875
NsikakApp <nsikakthompson73@gmail.com>
2021-07-07 19:57:09 +0100

    Initial commit

---
+++ app/src/main/java/com/nsikakthompson/api/ApiService.kt
@@ -0,0 +1,11 @@
+package com.nsikakthompson.api
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("https://app.ticketmaster.com/discovery/v2/events.json")
+    suspend fun getEvent(@Query("page") page: Int? = null,
+                         @Query("size")size: Int? = null,
+                         @Query("apiKey")  apiKey: String)
+}
