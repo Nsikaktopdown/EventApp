414476cfc93856234b721b45e4cf95d7e70be377
NsikakApp <nsikakthompson73@gmail.com>
2021-07-11 01:09:58 +0100

    add wish list counter menu

--- app/src/main/java/com/nsikakthompson/presentation/event_list/EventFragment.kt
+++ app/src/main/java/com/nsikakthompson/presentation/event_list/EventFragment.kt
@@ -1,12 +1,15 @@
 package com.nsikakthompson.presentation.event_list
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.MenuItemCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.nsikakthompson.R
 import com.nsikakthompson.databinding.FragmentEventsBinding
 import com.nsikakthompson.presentation.viewmodel.EventViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -22,27 +25,43 @@ class EventFragment : Fragment() {
         val binding = FragmentEventsBinding.inflate(inflater, container, false)
         context ?: return binding.root
 
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
+
         with(binding.rvEvents) {
             layoutManager = LinearLayoutManager(this.context)
             adapter = eventAdapter
         }
-
-
-
         subscribeUi(binding, eventAdapter)
 
-        setHasOptionsMenu(true)
+
+
         return binding.root
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
     private fun subscribeUi(binding: FragmentEventsBinding, adapter: EventAdapter) {
         viewModel.events.observe(viewLifecycleOwner, Observer { events ->
-            if(events.isNotEmpty()){
+            if (events.isNotEmpty()) {
                 binding.progressBar.visibility = View.GONE
                 eventAdapter.submitList(events)
             }
 
-
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_event, menu)
+        val item = menu.findItem(R.id.counter)
+         item.setActionView(R.layout.wishlist_counter)
+        val parent = item.actionView as ConstraintLayout
+        parent.findViewById<TextView>(R.id.tvCount).text = "3"
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 }
